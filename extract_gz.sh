a0e6fc90bc66e785d51f846b64f032b89a72492a
Han Zhao <han.zhao@shanda.com>
2025-07-13 19:20:00 +0800

    add script

---
+++ extract_gz.sh
@@ -0,0 +1,2 @@
+mkdir -p /root/autodl-tmp/decifier-new/data/mp-20/mp_full/serialized
+tar -xzvf /root/autodl-tmp/decifier-new/serialized.tar.gz -C /root/autodl-tmp/decifier-new/data/mp-20/mp_full/serialized
